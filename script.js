443984340afedda9f2b3473898d703a496443bb7
Mo Yameen Macchhar <myameen033@gmail.com>
2023-03-20 23:43:42 +0530

    cookies added

--- script.js
+++ script.js
@@ -8,78 +8,85 @@ const products = [{ name: 'Adida-1', brand: 'adidas', img: 'https://m.media-amaz
 { name: 'C', brand: 'puma', img: 'https://m.media-amazon.com/images/I/81G09nYZVIL._AC_UL320_.jpg', price: 8500 },
 { name: 'T', brand: 'puma', img: 'https://m.media-amazon.com/images/I/61uj2S1vOPL._AC_UL320_.jpg', price: 2500 },
 ]
-console.log(products)
+
 let tmp_p = products;
 let productlist = document.getElementById('product-list');
 
 
-function render(list) {
+function render() {
     productlist.innerHTML = '';
-    list.map((val) => {
-        productlist.innerHTML+=innerhtml(val);
+    tmp_p.map((val) => {
+        productlist.innerHTML += innerhtml(val);
     })
 }
 
-function innerhtml(val){
-    return `<li>
-                <img src=${val.img} />
-                <h3>${val.name}</h3>
-                <h4>${val.price}</h4>
-            </li>`
+function innerhtml(val) {
+    return `<div class="card">
+    <img src=${val.img} >
+    <h4>${val.name}</h4>
+    <h4>${val.brand}</h4>
+    <h4>${val.price}</h4>
+</div>`
 }
 
 function filter() {
     let filter = document.querySelectorAll('input[name="brand"]:checked');
     if (filter.length == 0) {
-        render(products);
+        tmp_p = products
+        srt('name');
+        render();
     }
     else {
-        document.getElementById('short').value='';
-        // s_val.value='';
-        tmp_p=[];
+        tmp_p = [];
         for (let i = 0; i < filter.length; i++) {
-            // console.log(filter[i].value);
             let tmp = products.filter((item) => item.brand === filter[i].value);
-            tmp_p=[...tmp_p,...tmp];
-            // tmp_p.push(tmp);
-            console.log(tmp_p)
-
-            // console.log(filterProduct);
-            render(tmp_p);
+            tmp_p = [...tmp_p, ...tmp];
+            srt('name');
+            document.getElementById('short').value = 'A-Z'
+            render();
         }
     }
 }
 
-function shortProducts(){
+function shortProducts() {
     let s_val = document.getElementById('short').value;
-    if(s_val=='by-name')
-    {
+    if (s_val == 'A-Z') {
         srt('name');
     }
-    if(s_val=='low-high')
-    {
+    if (s_val == 'Z-A') {
+        srt_desc('name');
+    }
+    if (s_val == 'low-high') {
         srt('price');
     }
-    if(s_val=='high-low')
-    {
+    if (s_val == 'high-low') {
         srt_desc('price')
     }
 }
 
-function srt(s_val)
-{
-    // console.log(s_val);
-    tmp_p = tmp_p.sort((a, b)=> a[s_val] < b[s_val] ? -1 : 1);
-    // console.log(s_product)
-    render(tmp_p);
+function srt(s_val) {
+    tmp_p = tmp_p.sort((a, b) => a[s_val] < b[s_val] ? -1 : 1);
+    render();
 }
 
-function srt_desc(s_val){
-    tmp_p = tmp_p.sort((a, b)=> a[s_val] > b[s_val] ? -1 : 1);
-    // console.log(s_product)
-    render(tmp_p);
+function srt_desc(s_val) {
+    tmp_p = tmp_p.sort((a, b) => a[s_val] > b[s_val] ? -1 : 1);
+    render();
+}
+
+function setcokie() {
+    const d = new Date();
+    d.setTime(d.getTime() + (20 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = 'popup' + "=" + 'true' + ";" + expires;
+    document.getElementById('pop-up').classList.add('pop-hide');
 }
 
 window.onload = () => {
-    render(products)
+    srt('name');
+    render()
+    let pop = document.cookie.indexOf('popup');
+    if (pop==-1) {
+        document.getElementById('pop-up').classList.remove('pop-hide')
+    }
 };
